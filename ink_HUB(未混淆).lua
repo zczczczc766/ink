325a7ed0b7755b09ab710a464242b1c429c110f0
zczczczc766 <2674134570@qq.com>
2026-08-06 14:05:05 +0800

    Add files via upload

--- ink_HUB(未混淆).lua
+++ ink_HUB(未混淆).lua
@@ -1012,7 +1012,7 @@ MusicTab:Button({
     end
 })
 
-local CosmeticsTab = D:Tab({Title="美化饰品", Icon="sparkles"})
+local CosmeticsTab = D:Tab({Title="美化饰品1", Icon="sparkles"})
 
 local player = game.Players.LocalPlayer
 local accessoryStates = {}
